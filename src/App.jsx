d30e8bc404e0019f85d960119c60fa9ad7dedb50
Adam Miller <adam.miller@csgi.com>
2021-05-13 16:26:39 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { useQuery, gql } from '@apollo/client';
+
+const QUERY = gql`
+  query PageData {
+  pages {
+    title
+    sections {
+      ... on BulletSection {
+        componentType
+        inverted
+        bulletPoints {
+          line1
+          line2
+          image {
+            url
+          }
+        }
+      }
+      ... on CallToActionSection {
+        componentType
+        line1
+        line2
+        line3
+        buttonText
+        comment
+      }
+      ... on TextSection {
+        componentType
+        line1
+        line2
+        image {
+          url
+        }        
+      }
+    }
+  }
+}
+
+`;
+
+function App() {
+ const { loading, error, data } = useQuery(QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <pre>{ JSON.stringify(data, null, 2) }</pre>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri:
+    'https://api-us-east-1.graphcms.com/v2/ckjlxpvg9epv701z0b07kfgqc/master',
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+    <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
